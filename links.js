6a38e7afaa7abfd40e9684e708155098e616a0dd
Ashley Ross <ashley.e.ross@gmail.com>
2016-11-08 16:20:05 -0800

    links file

---
+++ links.js
@@ -0,0 +1,261 @@
+
+var allFlights = '';
+var allLanguages = '';
+
+var romeWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=rome%2C+italy';
+var romeCurrency = '';
+var romeMaps = '';
+var romeCalendar = '';
+var romeEmbassy = '';
+
+var parisWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=paris%2C+france';
+var parisCurrency = '';
+var parisMaps = '';
+var parisCalendar = '';
+var parisEmbassy = '';
+
+var londonWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=london%2C+england';
+var londonCurrency = '';
+var londonMaps = '';
+var londonCalendar = '';
+var londonEmbassy = '';
+
+var amsterdamWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=amsterdam%2C+netherlands';
+var amsterdamCurrency = '';
+var amsterdamMaps = '';
+var amsterdamCalendar = '';
+var amsterdamEmbassy = '';
+
+var berlinWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=berlin%2C+germany';
+var berlinCurrency = '';
+var berlinMaps = '';
+var berlinCalendar = '';
+var berlinEmbassy = '';
+
+var budapestWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=budapest%2C+hungary';
+var budapestCurrency = '';
+var budapestMaps = '';
+var budapestCalendar = '';
+var budapestEmbassy = '';
+
+var tallinWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=london%2C+england';
+var tallinCurrency = '';
+var tallinMaps = '';
+var tallinCalendar = '';
+var tallinEmbassy = '';
+
+var kungsledenWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=kungslena%2C+sweden';
+var kungsledenCurrency = '';
+var kungsledenMaps = '';
+var kungsledenCalendar = '';
+var kungsledenEmbassy = '';
+
+var zermattWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=zermatt%2C+switzerland';
+var zermattCurrency = '';
+var zermattMaps = '';
+var zermattCalendar = '';
+var zermattEmbassy = '';
+
+var majorcaWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=london%2C+england';
+var majorcaCurrency = '';
+var majorcaMaps = '';
+var majorcaCalendar = '';
+var majorcaEmbassy = '';
+
+var pragueWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=prague%2C+czech+republic';
+var pragueCurrency = '';
+var pragueMaps = '';
+var pragueCalendar = '';
+var pragueEmbassy = '';
+
+var marmarisWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=marmaris%2C+turkey';
+var marmarisCurrency = '';
+var marmarisMaps = '';
+var marmarisCalendar = '';
+var marmarisEmbassy = '';
+
+var algarveWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=algarve%2C+portugal';
+var algarveCurrency = '';
+var algarveMaps = '';
+var algarveCalendar = '';
+var algarveEmbassy = '';
+
+var reykjavikWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=reykjavik%2C+iceland';
+var reykjavikCurrency = '';
+var reykjavikMaps = '';
+var reykjavikCalendar = '';
+var reykjavikEmbassy = '';
+
+var cavtatWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=cavtat%2C+croatia';
+var cavtatCurrency = '';
+var cavtatMaps = '';
+var cavtatCalendar = '';
+var cavtatEmbassy = '';
+
+var lasvegasWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=las+vegas%2C+nevada';
+var lasvegasCurrency = '';
+var lasvegasMaps = '';
+var lasvegasCalendar = '';
+var lasvegasEmbassy = '';
+
+var seattleWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=seattle%2C+wa';
+var seattleCurrency = '';
+var seattleMaps = '';
+var seattleCalendar = '';
+var seattleEmbassy = '';
+
+var sandiegoWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=san+diego%2C+california';
+var sandiegoCurrency = '';
+var sandiegoMaps = '';
+var sandiegoCalendar = '';
+var sandiegoEmbassy = '';
+
+var newyorkcityWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=new+york+%2C+new+york';
+var newyorkcityCurrency = '';
+var newyorkcityMaps = '';
+var newyorkcityCalendar = '';
+var newyorkcityEmbassy = '';
+
+var vancouverWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=vancouver%2C+canada';
+var vancouverCurrency = '';
+var vancouverMaps = '';
+var vancouverCalendar = '';
+var vancouverEmbassy = '';
+
+var cancunWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=cancun%2C+mexico';
+var cancunCurrency = '';
+var cancunMaps = '';
+var cancunCalendar = '';
+var cancunEmbassy = '';
+
+var burlingtonWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=burlington%2C+vermont';
+var burlingtonCurrency = '';
+var burlingtonMaps = '';
+var burlingtonCalendar = '';
+var burlingtonEmbassy = '';
+
+var riodejaneiroWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=rio+de+janeiro%2C+brazil';
+var riodejaneiroCurrency = '';
+var riodejaneiroMaps = '';
+var riodejaneiroCalendar = '';
+var riodejaneiroEmbassy = '';
+
+var chichenitzaWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=chichen-itza%2C+mexico';
+var chichenitzaCurrency = '';
+var chichenitzaMaps = '';
+var chichenitzaCalendar = '';
+var chichenitzaEmbassy = '';
+
+var limaWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=lima%2C+peru';
+var limaCurrency = '';
+var limaMaps = '';
+var limaCalendar = '';
+var limaEmbassy = '';
+
+var sanfranciscoWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=san+francisco%2C+california';
+var sanfranciscoCurrency = '';
+var sanfranciscoMaps = '';
+var sanfranciscoCalendar = '';
+var sanfranciscoEmbassy = '';
+
+var honoluluWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=honolulu%2C+hawaii';
+var honoluluCurrency = '';
+var honoluluMaps = '';
+var honoluluCalendar = '';
+var honoluluEmbassy = '';
+
+var victoriafallsWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=victoria+falls%2C+zimbabwe';
+var victoriafallsCurrency = '';
+var victoriafallsMaps = '';
+var victoriafallsCalendar = '';
+var victoriafallsEmbassy = '';
+
+var gizaWeather = 'https://www.wunderground.com/global/stations/62375.html';
+var gizaCurrency = '';
+var gizaMaps = '';
+var gizaCalendar = '';
+var gizaEmbassy = '';
+
+var baliWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=bali%2C+indonesia';
+var baliCurrency = '';
+var baliMaps = '';
+var baliCalendar = '';
+var baliEmbassy = '';
+
+var singaporeWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=singapore%2C+singapore';
+var singaporeCurrency = '';
+var singaporeMaps = '';
+var singaporeCalendar = '';
+var singaporeEmbassy = '';
+
+var rishikeshWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=rishikesh%2C+india';
+var rishikeshCurrency = '';
+var rishikeshMaps = '';
+var rishikeshCalendar = '';
+var rishikeshEmbassy = '';
+
+var phuketWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=phuket%2C+thailand';
+var phuketCurrency = '';
+var phuketMaps = '';
+var phuketCalendar = '';
+var phuketEmbassy = '';
+
+var hoianWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=hoi+an+vietnam';
+var hoianCurrency = '';
+var hoianMaps = '';
+var hoianCalendar = '';
+var hoianEmbassy = '';
+
+var luangprabangWeather = 'https://www.wunderground.com/q/zmw:00000.937.48930';
+var luangprabangoCurrency = '';
+var luangprabangMaps = '';
+var luangprabangCalendar = '';
+var luangprabangEmbassy = '';
+
+var siemreapWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=siem+reap%2C+cambodia';
+var siemreapCurrency = '';
+var siemreapMaps = '';
+var siemreapCalendar = '';
+var siemreapEmbassy = '';
+
+var baganWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=bagan%2C+myanmar';
+var baganCurrency = '';
+var baganMaps = '';
+var baganCalendar = '';
+var baganEmbassy = '';
+
+var cairnsWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=cairns%2C+australia';
+var cairnsCurrency = '';
+var cairnsMaps = '';
+var cairnsCalendar = '';
+var cairnsEmbassy = '';
+
+var queenstownWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=queenstown%2C+new+zealand';
+var queenstownCurrency = '';
+var queenstownMaps = '';
+var queenstownCalendar = '';
+var queenstownEmbassy = '';
+
+var chengduWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=chengdu%2C+china';
+var chengduCurrency = '';
+var chengduMaps = '';
+var chengduCalendar = '';
+var chengduEmbassy = '';
+
+var yongpyongWeather = 'https://www.wunderground.com/q/zmw:00000.144.47165';
+var yongpyongCurrency = '';
+var yongpyongMaps = '';
+var yongpyongCalendar = '';
+var yongpyongEmbassy = '';
+
+var kyotoWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=kyoto%2C+japan';
+var kyotoCurrency = '';
+var kyotoMaps = '';
+var kyotoCalendar = '';
+var kyotoEmbassy = '';
+
+var kualalumpurWeather = 'https://www.wunderground.com/cgi-bin/findweather/getForecast?query=kuala+lumpur%2C+malaysia';
+var kualalumpurCurrency = '';
+var kualalumpurMaps = '';
+var kualalumpurCalendar = '';
+var kualalumpurEmbassy = '';
