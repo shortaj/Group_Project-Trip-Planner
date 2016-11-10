d574d0d038b05dd87f63ace1e637185b430ed292
Ashley Ross <ashley.e.ross@gmail.com>
2016-11-10 10:58:44 -0800

    explore to card w open in new tab code

--- Explore.js
+++ Explore.js
@@ -3,17 +3,45 @@
 
 // var clickExplore = document.getElementById('explore');
 // clickExplore.addEventListener('click' , createExploreList);
+function handleCityClick(event) {
+  console.log('handleCityClick' , event.target.dataset.city);
+  localStorage.setItem('city', event.target.dataset.city);
 
 
-function createExploreList (event){
+}
+
+function createExploreList() {
   for (var i = 0; i < cityObjects.length; i++) {
     var cityUl = document.getElementById('cityUl');
     var cityLi = document.createElement('li');
-    var classText = cityObjects[i].city;
-    // cityLi.setAttribute('class' , classText);
-    cityLi.setAttribute('src' , cityObjects[i].path);
-    cityUl.appendChild(cityLi);
+    var cityLink = document.createElement('a');
+    var cityText = cityObjects[i].city;
+    var cityCountry = cityObjects[i].country;
+    cityLi.setAttribute('class', 'classStyle');
+    cityLi.dataset.city = JSON.stringify(cityObjects[i]);
+    cityLi.textContent = cityText + ', ' + cityCountry;
+    cityLink.setAttribute('href', 'card.html');
+    cityLink.addEventListener('click', handleCityClick);
+    // var goToCardPage = function(){
+    //   var image = document.getElementById('city-view');
+    //   image.setAttribute('src', cityObject[i].city);
+    //
+    //   var title = document.getElementById('blurb-title');
+    //   title.textContent = cityObject[i].city;
+    //
+    //   var weather = document.getElementById('weather-link');
+    //   weather.setAttribute('href', cityObject[i].weather);
+    //
+    //   var text = document.getElementById('blurb-text');
+    //   text.textContent = cityObject[i].describe;
+    //
+    // };
+    // cityLi.setAttribute('src' , );
+
+    cityUl.appendChild(cityLink);
+    cityLink.appendChild(cityLi);
     console.log('test');
   }
 }
+
 createExploreList();
